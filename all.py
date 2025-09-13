236c60f2d76ffabea02b0ff30d0e2068a70190e9
PRIYANKAKUTIYARE <139627672+PRIYANKAKUTIYARE@users.noreply.github.com>
2025-09-13 11:32:03 +0530

    Add files via upload

---
+++ all.py
@@ -0,0 +1,80 @@
+from math import *
+cook your dish here
+print("adjes")
+print("Priyanka Learning Python again for Placements")
+print("So let's start learning so that i won't stuck anywhere in PIs or Ots")
+print(("basic Python Program"))
+print("i will try my best to complete the whole syllabus together")
+
+print(". * . *")
+print(" * . * ")
+print(". * . *")
+
+ Variables and  Datatypes
+
+
+NUMBERS
+
+print(2*54*9-102)
+print(3*(4+1))
+print(3*4+1)
+my_num = 5
+print(str(my_num) + " My favorite Number")
+print(type(my_num))
+print(type(str(my_num)))
+z = 9.9332
+print(type(z))
+print(int(z))
+print(z)
+print(pow(4,2))
+print(max(4,5))
+print(min(5,6))
+print(round(z))
+print(floor(z))
+print(ceil(z))
+print(sqrt(36))
+
+
+a = "4"
+S = 4
+c = "John"
+
+print(c+" So lets do something here ")
+print(S)
+print(a)
+print("Let's do something very crazy thing here")
+
+STRING 
+print("Priyanka\nKutiyare")
+print("Priyanka\"Kutiyare")
+
+phrase = "Girraff Dodo"
+print(phrase + " is KP")
+print(phrase.lower())
+print(phrase.upper())
+print(len(phrase))
+print(phrase.islower())
+print(phrase[0])
+print(phrase[len(phrase)-1])
+print(phrase[1:4])
+print(phrase.index("D"))
+print(phrase.replace("Girraff", "Dodo"))
+print(phrase)
+
+for x in phrase:
+    print(x)
+
+# b = "Hello, World"
+# print(b[2:5])
+# print(b[1:])
+# print(b[:5])
+# print(b.strip())
+# print(b.split(","))
+
+# txt = "Pretty Girl"
+# print(txt)
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+# trt = f"nice thing to try at @ {price:.2f}"
+# print(trt)
